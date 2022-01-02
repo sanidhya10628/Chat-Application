98f597bf9e618d32897513745cdbb7985284fe3c
Sanidhya Mahajan <sanidhya10628@gmail.com>
2022-01-02 12:06:06 +0530

    rendering messages done

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,26 +1,73 @@
 const socket = io()
 
+// Elements
+const messageForm = document.getElementById('message-form')
+const messageFormInput = messageForm.querySelector('input')
+const messageFormButton = messageForm.querySelector('button')
+const sendLocationButton = document.getElementById('send-location')
+const messages = document.getElementById('messages')
+
+// Templates
+const messageTemplate = document.getElementById('message-template').innerHTML
+const locationMessageTemplate = document.getElementById('location-message-template').innerHTML
+
 socket.on('message', (message) => {
     console.log(message);
+    const html = Mustache.render(messageTemplate, {
+        message: message
+    })
+    messages.insertAdjacentHTML('beforeend', html)
 })
 
+socket.on('locationMessage', (url) => {
+    console.log(url);
+    const html = Mustache.render(locationMessageTemplate, {
+        url: url
+    })
 
-document.getElementById('message-form').addEventListener('submit', (e) => {
+    messages.insertAdjacentHTML('beforeend', html)
+})
+
+messageForm.addEventListener('submit', (e) => {
     e.preventDefault()
+
+    // disable
+    messageFormButton.setAttribute('disabled', 'disabled')
     const message = e.target.elements.message.value
-    socket.emit('sendMessage', message)
+    socket.emit('sendMessage', message, (error) => {
+
+        // enable
+        messageFormButton.removeAttribute('disabled')
+        messageFormInput.value = ''
+        messageFormInput.focus()
+
+        if (error) {
+            return console.log(error);
+        }
+
+        console.log('Message Delivered');
+    })
 })
 
 
-document.getElementById('send-location').addEventListener('click', () => {
+sendLocationButton.addEventListener('click', () => {
     if (!navigator.geolocation) {
         return alert('Geolocation is not supported by your browser.')
     }
+
+    // disable
+    sendLocationButton.setAttribute('disabled', 'disabled')
 
     navigator.geolocation.getCurrentPosition((position) => {
         socket.emit('sendLocation', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
+        }, () => {
+
+            // enable
+            sendLocationButton.removeAttribute('disabled')
+
+            console.log('Location Shared');
         })
     })
 
