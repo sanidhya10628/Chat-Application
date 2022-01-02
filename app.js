6914f74a201be17ab52f1f9f7c8e3a164e2e4045
Sanidhya Mahajan <sanidhya10628@gmail.com>
2022-01-02 10:23:14 +0530

    geolocation done

--- app.js
+++ app.js
@@ -27,9 +27,16 @@ io.on('connection', (socket) => {
         io.emit('message', message)
     })
 
+    socket.on('sendLocation', (coords) => {
+        io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+    })
+
+
     socket.on('disconnect', () => {
         io.emit('message', 'A user has left!')
     })
+
+
 })
 
 // Listen On PORT
